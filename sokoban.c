54f16f7ada673546e5f33faec0d9c8eb57f62605
ssukimbeomjun <wka8963@naver.com>
2017-05-24 11:14:21 +0900

    이름 추가 + 움직일 때 버그수정
    이름추가  + 움직일 때 버그 수정

--- sokoban.c
+++ sokoban.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <termio.h>
+#include <stdlib.h>
 
+#define name_max_length 10  //이름의 최대 길이
 #define map_height 11  //맵의 세로 길이
-#define map_width 23  //맵의 가로 길이
+#define map_width 24  //맵의 가로 길이
+
+int scan_name(char name[name_max_length]);  //이름을 적기 위한 함수, 리턴값은 이름의 길이
 
 int getch(void); //엔터 없이 입력하게 하기 위한 함수
 
@@ -20,9 +24,13 @@ int main(void)
 {
       FILE * map_load;  //맵 파일 불러오기 위한 파일 변수
       char map_file[map_height][map_width];  //맵 파일을 저장하기 위한 변수
-      map_load = fopen("map", "r");  //맵 파일 불러오기
+      char name[name_max_length]; //이름을 입력받는 함수
       char insert;  //입력을 입력받는 변수
+      int name_length;
 
+      name_length = scan_name(name);
+
+      map_load = fopen("map", "r");  //맵 파일 불러오기
       for(int i=0;i<map_height;i++) //맵 파일을 map_file에 저장
       {
         for(int j=0;j<map_width;j++)
@@ -30,8 +38,10 @@ int main(void)
           fscanf(map_load, "%c", &map_file[i][j]);
         }
       }
+
       print_map(map_file);
       printf("(Command)\n");  //맵파일을 출력한 뒤 값을 입력받기 위해 개행
+
       while(1)  //값을 입력받는다(e를 누르기 전까지)
       {
         insert = getch();  //값을 입력 받는다
@@ -110,7 +120,27 @@ int find_char_width(char map_file[][map_width])
 
 void move_down(char map_file[][map_width], int height, int width)
 {
-  if(map_file[height+1][width] != '#')
+  if(map_file[height+2][width] == '0' && map_file[height+1][width] == '$')
+  {
+    char temp;
+    map_file[height+2][width] = ' ';
+    temp = map_file[height+2][width];
+    map_file[height+2][width] = map_file[height+1][width];
+    map_file[height+1][width] = temp;
+    temp = map_file[height+1][width];
+    map_file[height+1][width] = map_file[height][width];
+    map_file[height][width] = temp;
+  }
+  else if(map_file[height+1][width] == '$')
+  {
+      char temp = map_file[height+2][width];
+      map_file[height+2][width] = map_file[height+1][width];
+      map_file[height+1][width] = temp;
+      temp = map_file[height+1][width];
+      map_file[height+1][width] = map_file[height][width];
+      map_file[height][width] = temp;
+  }
+  else if(map_file[height+1][width] != '#')
   {
     char temp = map_file[height+1][width];
     map_file[height+1][width] = map_file[height][width];
@@ -120,17 +150,58 @@ void move_down(char map_file[][map_width], int height, int width)
 
 void move_top(char map_file[][map_width], int height, int width)
 {
-  if(map_file[height-1][width] != '#')
+  char temp;
+  if(map_file[height-2][width] == '0' && map_file[height-1][width] == '$')
   {
-    char temp = map_file[height-1][width];
+    char temp;
+    map_file[height-2][width] = ' ';
+    temp = map_file[height-2][width];
+    map_file[height-2][width] = map_file[height-1][width];
+    map_file[height-1][width] = temp;
+    temp = map_file[height-1][width];
     map_file[height-1][width] = map_file[height][width];
     map_file[height][width] = temp;
+  }
+  else if(map_file[height-1][width] == '$')
+  {
+      char temp = map_file[height-2][width];
+      map_file[height-2][width] = map_file[height-1][width];
+      map_file[height-1][width] = temp;
+      temp = map_file[height-1][width];
+      map_file[height-1][width] = map_file[height][width];
+      map_file[height][width] = temp;
+  }
+  else if(map_file[height-1][width] != '#')
+  {
+      char temp = map_file[height-1][width];
+      map_file[height-1][width] = map_file[height][width];
+      map_file[height][width] = temp;
   }
 }
 
 void move_right(char map_file[][map_width], int height, int width)
 {
-  if(map_file[height][width+1] != '#')
+  if(map_file[height][width+2] == '0' && map_file[height][width+1] == '$')
+  {
+    char temp;
+    map_file[height][width+2] = ' ';
+    temp = map_file[height][width+2];
+    map_file[height][width+2] = map_file[height][width+1];
+    map_file[height][width+1] = temp;
+    temp = map_file[height][width+1];
+    map_file[height][width+1] = map_file[height][width];
+    map_file[height][width] = temp;
+  }
+  else if(map_file[height][width+1] == '$')
+  {
+    char temp = map_file[height][width+2];
+    map_file[height][width+2] = map_file[height][width+1];
+    map_file[height][width+1] = temp;
+    temp = map_file[height][width+1];
+    map_file[height][width+1] = map_file[height][width];
+    map_file[height][width] = temp;
+  }
+  else if(map_file[height][width+1] != '#')
   {
     char temp = map_file[height][width+1];
     map_file[height][width+1] = map_file[height][width];
@@ -140,10 +211,46 @@ void move_right(char map_file[][map_width], int height, int width)
 
 void move_left(char map_file[][map_width], int height, int width)
 {
-  if(map_file[height][width-1] != '#')
+  if(map_file[height][width-2] == '0' && map_file[height][width-1] == '$')
+  {
+    char temp;
+    map_file[height][width-2] = ' ';
+    temp = map_file[height][width-2];
+    map_file[height][width-2] = map_file[height][width-1];
+    map_file[height][width-1] = temp;
+    temp = map_file[height][width-1];
+    map_file[height][width-1] = map_file[height][width];
+    map_file[height][width] = temp;
+  }
+  else if(map_file[height][width-1] == '$')
+  {
+    char temp = map_file[height][width-2];
+    map_file[height][width-2] = map_file[height][width-1];
+    map_file[height][width-1] = temp;
+    temp = map_file[height][width-1];
+    map_file[height][width-1] = map_file[height][width];
+    map_file[height][width] = temp;
+  }
+  else if(map_file[height][width-1] != '#')
   {
     char temp = map_file[height][width-1];
     map_file[height][width-1] = map_file[height][width];
     map_file[height][width] = temp;
   }
+}
+
+int scan_name(char name[name_max_length])
+{
+  int i_name=0;  //이름 길이
+  printf("Start.....\n");    //이름을 입력받기 위한 것
+  printf("Input name : ");
+
+	for ( i_name=0; i_name < name_max_length ; i_name++)
+  {
+     scanf("%c", &name[i_name]);
+		 if( name[i_name] == '\n')		//(enter) 개행 문자를 받을시 탈출
+			break;
+	}
+
+  return i_name;
 }
